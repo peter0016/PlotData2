2366756703052abac32bd4ba6ce1519303f9a9f5
peter0016 <61506765+peter0016@users.noreply.github.com>
2020-08-21 09:32:00 +0200

    Update funx.py

--- funx.py
+++ funx.py
@@ -112,6 +112,7 @@ def plotdfs_inoneplot(dfs, startdate,enddate):
     #df.head()
     dfloc0=dfs[0].loc[startdate:enddate]
     dfloc1=dfs[1].loc[startdate:enddate]
+    dfloc2=dfs[2].loc[startdate:enddate]
     dflen=len(dfloc0.columns)
     fig, ax=plt.subplots(dflen)
     #ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
@@ -120,19 +121,22 @@ def plotdfs_inoneplot(dfs, startdate,enddate):
     #print(col.find('_p'))
         if col.find('_p')==-1:
             dfloc0[col].plot(sharex=plt.gca(),color=colortable[i],label=col,linewidth=1,grid=True)
-            dfloc1[col].plot(sharex=plt.gca(),color=colortable[i],label=col,linewidth=1,grid=True,alpha=0.35)
-            
+            dfloc1[col].plot(sharex=plt.gca(),color=colortable[i],label=col,linewidth=1,grid=True,alpha=0.25)
+            dfloc2[col].plot(sharex=plt.gca(),color=colortable[i],label=col,linewidth=1,grid=True,alpha=0.4)
         else:          
             dfloc0[col].plot(sharex=plt.gca(),
                 color=colortable[i],marker='|',label=col,grid=True,linestyle=None)
             dfloc1[col].plot(sharex=plt.gca(),
-                color=colortable[i],marker='|',label=col,grid=True,linestyle=None,alpha=0.35)
+                color=colortable[i],marker='|',label=col,grid=True,linestyle=None,alpha=0.25)
+            dfloc2[col].plot(sharex=plt.gca(),
+                color=colortable[i],marker='|',label=col,grid=True,linestyle=None,alpha=0.25)
         #plt.legend(loc="lower left")
         #
         #plt.legend(('string1','string2'),loc="lower left")
         #print(str(dfloc0.columns[i])+' Logger_01')
         plt.legend((str(dfloc0.columns[i])+' L01',
-                      str(dfloc1.columns[i])+' L02'),loc="lower left")
+                      str(dfloc1.columns[i])+' L02',
+                       str(dfloc1.columns[i])+' L02'),loc="lower left")
 
 def getdfs_frommulticsv(ndays,fileend='.txt'):
     urls=["https://uni-wuppertal.sciebo.de/s/0WBFg4RTw4I428O/download?path=%2F&files=",
